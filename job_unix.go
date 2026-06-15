9184735d03c8ba4cef65f3943c9b3c8cddbce859
sammy-ette <torchedsammy@gmail.com>
2026-06-15 09:05:59 -0400

    fix: handle errors on job foreground

--- job_unix.go
+++ job_unix.go
@@ -14,14 +14,25 @@ var bgProcAttr *syscall.SysProcAttr = &syscall.SysProcAttr{
 }
 
 func (j *job) foreground() error {
-	pgid, _ := syscall.Getpgid(j.pid)
-	// tcsetpgrp
-	unix.IoctlSetPointerInt(0, unix.TIOCSPGRP, pgid)
+	pgid, err := syscall.Getpgid(j.pid)
+	if err != nil {
+		return err
+	}
+
+	hshPgid, err := syscall.Getpgid(os.Getpid())
+	if err != nil {
+		return err
+	}
+
+	// tcsetpgrp - give the job's process group control of the terminal
+	if err := unix.IoctlSetPointerInt(0, unix.TIOCSPGRP, pgid); err != nil {
+		return err
+	}
+	// always give control of the terminal back to hilbish afterwards
+	defer unix.IoctlSetPointerInt(0, unix.TIOCSPGRP, hshPgid)
+
 	proc, _ := os.FindProcess(j.pid)
 	proc.Wait()
-
-	hshPgid, _ := syscall.Getpgid(os.Getpid())
-	unix.IoctlSetPointerInt(0, unix.TIOCSPGRP, hshPgid)
 
 	return nil
 }
