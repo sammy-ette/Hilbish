9dc54ac83ce14d4aeae7a0dd8c6b978624a6b31b
sammy-ette <torchedsammy@gmail.com>
2026-06-15 08:59:51 -0400

    fix: share require paths on unix

--- vars_darwin.go
+++ vars_darwin.go
@@ -4,16 +4,7 @@ package main
 
 // String vars that are free to be changed at compile time
 var (
-	requirePaths = commonRequirePaths + `.. ';'
-	.. hilbish.dataDir .. '/libs/?/init.lua;'
-	.. hilbish.dataDir .. '/libs/?/?.lua;'` + macosUserPaths
-	macosUserPaths = `
-	.. hilbish.userDir.data     .. '/hilbish/libs/?/init.lua;'
-	.. hilbish.userDir.data	    .. '/hilbish/libs/?/?.lua;'
-	.. hilbish.userDir.data	    .. '/hilbish/libs/?.lua;'
-	.. hilbish.userDir.config	.. '/hilbish/?/init.lua;'
-	.. hilbish.userDir.config	.. '/hilbish/?/?.lua;'
-	.. hilbish.userDir.config	.. '/hilbish/?.lua'`
+	requirePaths   = unixRequirePaths
 	dataDir        = "/usr/local/share/hilbish"
 	defaultConfDir = getenv("XDG_CONFIG_HOME", "~/.config")
 )
