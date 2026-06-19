0ec8ac7bfa275ab3749ef1b11d9344af15cd0128
sammy-ette <torchedsammy@gmail.com>
2026-06-19 14:15:24 -0400

    feat: port terminal

--- golibs/terminal/terminal.go
+++ golibs/terminal/terminal.go
@@ -3,6 +3,8 @@
 package terminal
 
 import (
+	"os"
+
 	"hilbish/moonlight"
 
 	"golang.org/x/term"
@@ -12,12 +14,10 @@ var termState *term.State
 
 func Loader(rtm *moonlight.Runtime) moonlight.Value {
 	exports := map[string]moonlight.Export{
-		/*
-			"setRaw": {termsetRaw, 0, false},
-			"restoreState": {termrestoreState, 0, false},
-			"size": {termsize, 0, false},
-			"saveState": {termsaveState, 0, false},
-		*/
+		"setRaw":       {Function: termsetRaw, ArgNum: 0, Variadic: false},
+		"restoreState": {Function: termrestoreState, ArgNum: 0, Variadic: false},
+		"size":         {Function: termsize, ArgNum: 0, Variadic: false},
+		"saveState":    {Function: termsaveState, ArgNum: 0, Variadic: false},
 	}
 
 	mod := moonlight.NewTable()
@@ -26,54 +26,53 @@ func Loader(rtm *moonlight.Runtime) moonlight.Value {
 	return moonlight.TableValue(mod)
 }
 
-/*
 // size()
 // Gets the dimensions of the terminal. Returns a table with `width` and `height`
 // NOTE: The size refers to the amount of columns and rows of text that can fit in the terminal.
-func termsize(mlr *moonlight.Runtime, c *moonlight.GoCont) (moonlight.Cont, error) {
+func termsize(mlr *moonlight.Runtime) error {
 	w, h, err := term.GetSize(int(os.Stdin.Fd()))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	dimensions := moonlight.NewTable()
 	dimensions.SetField("width", moonlight.IntValue(int64(w)))
 	dimensions.SetField("height", moonlight.IntValue(int64(h)))
 
-	return mlr.PushNext1(c, moonlight.TableValue(dimensions)), nil
+	mlr.PushNext1(moonlight.TableValue(dimensions))
+	return nil
 }
 
 // saveState()
 // Saves the current state of the terminal.
-func termsaveState(mlr *moonlight.Runtime, c *moonlight.GoCont) (moonlight.Cont, error) {
+func termsaveState(mlr *moonlight.Runtime) error {
 	state, err := term.GetState(int(os.Stdin.Fd()))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	termState = state
-	return c.Next(), nil
+	return nil
 }
 
 // restoreState()
 // Restores the last saved state of the terminal
-func termrestoreState(mlr *moonlight.Runtime, c *moonlight.GoCont) (moonlight.Cont, error) {
+func termrestoreState(mlr *moonlight.Runtime) error {
 	err := term.Restore(int(os.Stdin.Fd()), termState)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return c.Next(), nil
+	return nil
 }
 
 // setRaw()
 // Puts the terminal into raw mode.
-func termsetRaw(mlr *moonlight.Runtime, c *moonlight.GoCont) (moonlight.Cont, error) {
+func termsetRaw(mlr *moonlight.Runtime) error {
 	_, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return c.Next(), nil
+	return nil
 }
-*/
