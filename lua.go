b7c94e87725efeee73ddef54b4151151ab59f7d4
sammy-ette <torchedsammy@gmail.com>
2026-06-19 14:26:04 -0400

    refactor: sync lua.go with master, with moonlight changes

--- lua.go
+++ lua.go
@@ -18,45 +18,17 @@ import (
 
 func luaInit() {
 	l = moonlight.NewRuntime()
-	println("runtime init")
 
-	l.LoadLibrary(hilbishLoader, "hilbish")
-	// yes this is stupid, i know
-	l.DoString("hilbish = require 'hilbish'")
-	println("hilbish mod init")
-
-	hooks = bait.New(l)
-	hooks.SetRecoverer(func(event string, handler *bait.Listener, err any) {
-		fmt.Println("Error in `error` hook handler:", err)
-		hooks.Off(event, handler)
-	})
-	l.LoadLibrary(hooks.Loader, "bait")
-	println("bait init")
-
-	l.LoadLibrary(fs.Loader, "fs")
-	println("fs init")
-	l.LoadLibrary(terminal.Loader, "terminal")
-	println("terminal init")
-	l.LoadLibrary(snail.Loader, "snail")
-	println("snail init")
-	l.LoadLibrary(readline.Loader, "readline")
-	println("readline init")
-
-	cmds = commander.New(l)
-	l.LoadLibrary(cmds.Loader, "commander")
-	println("commander init")
-
-	/*
-		yarnPool := yarn.New(yarnloadLibs)
-		lib.LoadLibs(l.UnderlyingRuntime(), yarnPool.Loader)
-	*/
-
+	loadLibs(l)
 	luaArgs := moonlight.NewTable()
 	for i, arg := range getopt.Args() {
 		luaArgs.Set(moonlight.IntValue(int64(i)), moonlight.StringValue(arg))
 	}
+
 	l.GlobalTable().Set(moonlight.StringValue("args"), moonlight.TableValue(luaArgs))
-	println("set cmd line args")
+
+	// yarnPool := yarn.New(yarnloadLibs)
+	// l.LoadLibrary(yarnPool.Loader, "yarn")
 
 	// Add more paths that Lua can require from
 	_, err := l.DoString("package.path = package.path .. " + requirePaths)
@@ -64,10 +36,8 @@ func luaInit() {
 		fmt.Fprintln(os.Stderr, "Could not add Hilbish require paths! Libraries will be missing. This shouldn't happen.")
 	}
 
-	println("running config")
 	err1 := l.DoFile("nature/init.lua")
 	if err1 != nil {
-		fmt.Println(err1)
 		err2 := l.DoFile(filepath.Join(dataDir, "nature", "init.lua"))
 		if err2 != nil {
 			fmt.Fprintln(os.Stderr, "Missing nature module, some functionality and builtins will be missing.")
@@ -75,4 +45,43 @@ func luaInit() {
 			fmt.Fprintln(os.Stderr, "global install error:", err2)
 		}
 	}
+}
+
+func loadLibs(r *moonlight.Runtime) {
+	l.LoadLibrary(hilbishLoader, "hilbish")
+	// yes this is stupid, i know
+	l.DoString("hilbish = require 'hilbish'")
+
+	hooks = bait.New(r)
+	hooks.SetRecoverer(func(event string, handler *bait.Listener, err interface{}) {
+		fmt.Println("Error in `error` hook handler:", err)
+		hooks.Off(event, handler)
+	})
+	l.LoadLibrary(hooks.Loader, "bait")
+
+	// Add Ctrl-C handler
+	hooks.On("signal.sigint", func(...interface{}) moonlight.Value {
+		if !interactive {
+			os.Exit(0)
+		}
+		return moonlight.NilValue
+	})
+
+	l.LoadLibrary(fs.Loader, "fs")
+	l.LoadLibrary(terminal.Loader, "terminal")
+	l.LoadLibrary(snail.Loader, "snail")
+
+	cmds = commander.New(r)
+	l.LoadLibrary(cmds.Loader, "commander")
+	l.LoadLibrary(readline.Loader, "readline")
+}
+
+func yarnloadLibs(r *moonlight.Runtime) {
+	l.LoadLibrary(hilbishLoader, "hilbish")
+	l.LoadLibrary(hooks.Loader, "bait")
+	l.LoadLibrary(fs.Loader, "fs")
+	l.LoadLibrary(terminal.Loader, "terminal")
+	l.LoadLibrary(snail.Loader, "snail")
+	l.LoadLibrary(cmds.Loader, "commander")
+	l.LoadLibrary(readline.Loader, "readline")
 }
